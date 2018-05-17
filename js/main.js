39184ba88ba4725ee0166c58c3da3ac4c1aa558c
danushkao <web.leush.daniil@gmail.com>
2018-05-17 19:34:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+;(function($) {
+	"use strict";
+	$(function() {
+		var $fullScreenSlider = $('.ba-slider');
+		$fullScreenSlider.slick({
+			dots: true,
+			infinite: false
+		});
+
+
+
+	});
+
+
+})(jQuery);
+
